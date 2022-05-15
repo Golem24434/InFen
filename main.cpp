e4d5dba7b842e0230c6d7c01db090ef5df6aabcd
Maxim Davydov <norm.pochta.maxa@gmail.com>
2022-05-15 18:36:31 +0300

    Init commit

--- main.cpp
+++ main.cpp
@@ -9,36 +9,6 @@
 
 using namespace Qt;
 
-int main()
-{
-    QTextStream in(stdin);
-    QTextStream out(stdout);
+int main() {
 
-    const QString imgDir = "C:\\Users\\normp\\Documents\\Programming\\Technopark\\Chess in FEN\\Images\\";
-    QString imgName;
-    QImage img;
-
-    out << "Input picture name: " << endl;
-    in >> imgName;
-    img.load(imgDir + imgName);
-
-    if (img.isNull()) {
-        out << "Cannot open image" << endl;
-        return 1;
-    } else {
-        out << "Image loaded successfully" << endl;
-    }
-
-    QImage normedImg = normalizeImage(img);
-
-    std::vector<std::vector<int>> objects = imgParse(normedImg);
-
-    if (objects.empty()) {
-        out << "Cannot parse image" << endl;
-        return 1;
-    } else {
-        out << "Image parsed successfully" << endl;
-    }
-
-    return 0;
 }
