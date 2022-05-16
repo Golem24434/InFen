de25936060058a2635feb5940ee04583e40fef52
Gleb <glebkos777@gmail.com>
2022-05-16 18:20:00 +0300

    FEN_maker done

---
+++ FEN_code_maker/include/FEN_maker.hpp
@@ -0,0 +1,10 @@
+#ifndef _FEN_MAKER_H_
+#define _FEN_MAKER_H_
+
+#include <iostream>
+
+using namespace std;
+
+string FEN_maker(char[8][8]);
+
+#endif // _FEN_MAKER_H_
---
+++ FEN_code_maker/src/FEN_maker.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+#include "../include/FEN_maker.hpp"
+
+#define EMPTY_PLACE '0'
+
+using namespace std;
+
+string FEN_maker(vector <char> matrix) {
+    string answer = "";
+    for (int i = 0; i < 8; ++i) {
+        int count = 0;
+        for (int j = 0; j < 8; ++j) {
+            if (matrix[i * 8 + j] == EMPTY_PLACE) {
+                count++;
+            } else {
+                if (count != 0) {
+                    answer.push_back((char)(count + 48));
+                }
+                count = 0;
+            }
+        }
+        answer.push_back('/');
+    }
+    answer.push_back(' ');
+    answer.push_back('w');
+    answer.push_back(' ');
+    answer.push_back('-');
+    answer.push_back(' ');
+    answer.push_back('0');
+    answer.push_back(' ');
+    answer.push_back('1');
+    return answer;
+}
